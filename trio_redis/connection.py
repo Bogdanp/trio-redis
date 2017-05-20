c5cd933ee00f5254add4ec16ebfeda138a8de3a3
Bogdan Popa <popa.bogdanp@gmail.com>
2017-05-20 18:17:30 +0300

    refactor: inline streaming parser into RedisConnection

--- trio_redis/connection.py
+++ trio_redis/connection.py
@@ -13,8 +13,16 @@ AP = ord("*")
 known_prefixes = {SP, EP, IP, BP, AP}
 
 
+class ReadMore(Exception):
+    """Raised by parse to signal that it needs more data.
+    """
+
+
 class RedisConnection:
     """This class faciliates all communication with Redis via a trio socket.
+
+    Warning:
+      The interface of this class may change at any time, without notice.
 
     Parameters:
       addr(bytes)
@@ -48,59 +56,60 @@ class RedisConnection:
 
     async def process_response(self):
         data = await self.sock.recv(self.bufsize)
-        if not data or data[0] not in known_prefixes:
-            raise ProtocolError(f"Unexpected response from Redis: {data!r}.")
+        while True:
+            try:
+                item, _ = await self.parse(data)
+                return item
+            except ReadMore:
+                data += await self.sock.recv(self.bufsize)
+
+    async def parse(self, data):
+        try:
+            index = data.index(b"\r\n")
+        except ValueError:
+            raise ReadMore()
+
+        if data[0] not in known_prefixes:
+            raise ProtocolError(f"Unexpected data in response: {data!r}.")
 
         elif data[0] == SP:
-            return await self.process_simple_string(data)
+            return data[1:index], data[index + 2:]
 
         elif data[0] == EP:
-            return await self.process_error_string(data)
+            error = data[1:index].decode("ascii")
+            if error.startswith("WRONGTYPE"):
+                raise ResponseTypeError(error[len("WRONGTYPE "):])
+
+            elif error.startswith("ERR"):
+                raise ResponseError(error[len("ERR "):])
+
+            else:
+                raise ResponseError(error)
 
         elif data[0] == IP:
-            return int(await self.process_simple_string(data))
+            return int(data[1:index]), data[index + 2:]
 
         elif data[0] == BP:
-            return await self.process_bulk_string(data)
+            length, data = int(data[1:index]), data[index + 2:]
+            if length == -1:
+                return None, data
+
+            elif len(data) < length + 2:
+                raise ReadMore()
+
+            return data[:length], data[length + 2:]
 
         elif data[0] == AP:
-            return await self.process_array(data)
+            length, data = int(data[1:index]), data[index + 2:]
+            if length == -1:
+                return None, data
 
-        else:
+            return await self.parse_array(length, data)
+
+        else:  # pragma: no cover
             assert False, "unreachable"
 
-    async def process_simple_string(self, data):
-        while not data.endswith(b"\r\n"):
-            data += await self.sock.recv(self.bufsize)
-
-        return data[1:-2]
-
-    async def process_error_string(self, data):
-        error = (await self.process_simple_string(data)).decode("ascii")
-        if error.startswith("WRONGTYPE"):
-            raise ResponseTypeError(error[len("WRONGTYPE "):])
-
-        elif error.startswith("ERR"):
-            raise ResponseError(error[len("ERR "):])
-
-        else:
-            raise ResponseError(error)
-
-    async def process_bulk_string(self, data):
-        length, data = extract_length(data)
-        if length == -1:
-            return None
-
-        while len(data) < length + 2:
-            data += await self.sock.recv(self.bufsize)
-
-        return data[:-2]
-
-    async def process_array(self, data):
-        length, data = extract_length(data)
-        if length == -1:
-            return None
-
+    async def parse_array(self, length, data):
         items = []
         while len(items) < length:
             if not data:
@@ -108,61 +117,9 @@ class RedisConnection:
                 continue
 
             try:
-                item, data = parse(data)
+                item, data = await self.parse(data)
                 items.append(item)
             except ReadMore:
                 data += await self.sock.recv(self.bufsize)
 
-        return items
-
-
-class ReadMore(Exception):
-    """Raised by parse to signal that it needs more data to parse.
-    """
-
-
-def parse(data):
-    try:
-        index = data.index(b"\r\n")
-    except ValueError:
-        raise ReadMore()
-
-    if data[0] not in known_prefixes:
-        raise ProtocolError(f"Unexpected data in array response: {data!r}.")
-
-    elif data[0] == SP:
-        return data[1:index], data[index + 2:]
-
-    elif data[0] == IP:
-        return int(data[1:index]), data[index + 2:]
-
-    elif data[0] == BP:
-        length, data = extract_length(data)
-        if length == -1:
-            return None
-
-        elif len(data) < length + 2:
-            raise ReadMore()
-
-        return data[:length], data[length + 2:]
-
-    elif data[0] == AP:
-        length, data = extract_length(data)
-        if length == -1:
-            return None
-
-        items = []
-        while len(items) < length:
-            item, data = parse(data)
-            items.append(item)
-
-        return items
-
-    else:
-        assert False, "unreachable"
-
-
-def extract_length(data):
-    for i, c in enumerate(data[1:], 1):
-        if c == 13:
-            return int(data[1:i]), data[i + 2:]
+        return items, data
